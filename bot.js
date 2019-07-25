16c6c9f753071ec2722923ff08668ff67db0eb30
wilburforce83 <wilburforce83@gmail.com>
2019-07-25 11:12:11 +0100

    recoding whole thing to make it work properly

--- bot.js
+++ bot.js
@@ -1,96 +1,79 @@
-// Debug flag
+// Developed by basilisk for use as a twitterbot boilerplate to help clients increase exposure by quote tweeting from a range of hashtags
+// and a range of canned responses with @mentions to increase traffic through their sites.
 
 var debug = true;
-
-// Twitter library
 var Twit = require('twit')
-
-// We need to include our configuration file
 var T = new Twit(require('./config.js'))
+var myScreenName = 'binarybotrading';
 
-// Array of hashtags to randomly search for on each cycle of the bot:
-var hashtags = ['#binarybot', '#autotrading', '#binaryoptions', '#tradingbot']
+// Array of hashtags or usernames to randomly search for on each cycle of the bot:
+var hashtags = ['#binarybot', '#autotrading', 'binarydotcom', '#binarytrading', '#iqoption', '#binaryoptionsignals', '#binarysignals', '#binarysignal', '#tradingbot'] // A list of as may hashtags as you like.
 
+//status text for quoted tweets (array of canned responses keep vague but different enough to make the bot seem more real life)
+var quoteComment = [
+  'Try our mobile autotrader for @Binarydotcom | http://bit.ly/2K0GArX #binarybot #autotrading',
+  'Try our autotrading app for @Binarydotcom | http://bit.ly/2Y10EUI #binaryoptions #makemoneyonline',
+  'Join our binarybot community for @Binarydotcom https://discord.gg/mEXmWvH #binarytrading #binarybot #makemoneyonline #binarysignals',
+  'Start robotic trading for free with bbTrader (http://bit.ly/2Y10EUI) powered by @Binarydotcom',
+  'Use our totally free mobile trading bot for @Binarydotcom (http://bit.ly/2K0GArX) starting #makingmoneyonline now! #binarytrading',
+  'Be part of our binary bot movement! Free autotrading for mobile and web, forever! https://discord.gg/mEXmWvH #makemoneyonline #binaryoptions #trading'
 
-/*
-// A user stream
-var stream = T.stream('user')
-// When someone follows the user
-stream.on('follow', followed)
-stream.on('tweet', tweetEvent)
-
-// In this callback we can see the name and screen name
-function followed(event) {
-  var name = event.source.name
-  var screenName = event.source.screen_name
-  var response = 'Thanks for following bbTrader, ' + name + ' @' + screenName + '! why not checkout https://webtrader.binarybottrading.eu or https://bbmobile.binarybottrading.eu !!'
-  // Post that tweet!
-  T.post('statuses/update', {
-    status: response
-  }, tweeted)
-
-  console.log('I was followed by: ' + name + ' @' + screenName)
-}
-
-// Here a tweet event is triggered!
-function tweetEvent(tweet) {
-  // If we wanted to write a file out
-  // to look more closely at the data
-  // var fs = require('fs')
-  // var json = JSON.stringify(tweet,null,2)
-  // fs.writeFile("tweet.json", json, output)
-
-  // Who is this in reply to?
-  var reply_to = tweet.in_reply_to_screen_name
-  // Who sent the tweet?
-  var name = tweet.user.screen_name
-  // What is the text?
-  var txt = tweet.text
-
-  // Ok, if this was in reply to me
-  // Replace selftwitterhandle with your own twitter handle
-  console.log(reply_to, name, txt)
-  if (reply_to === 'selftwitterhandle') {
-
-    // Get rid of the @ mention
-    txt = txt.replace(/@selftwitterhandle/g, '')
-
-    // Start a reply back to the sender
-    var reply = 'Hi @' + name + ' ' + ', Thanks for the mention, why not joinour discord: https://discord.gg/xVtsakq'
-
-    console.log(reply)
-    // Post that tweet!
-    T.post('statuses/update', {
-      status: reply
-    }, tweeted)
-  }
-}
-*/
+];
 
 // This function finds the latest tweet with the #hashtag, and retweets it.
 function retweetLatest() {
 
   // Randomly select the hashtag to search
-  var hash = hashtags[Math.floor(Math.random() * hashtags.length)]
+  var hash = hashtags[Math.floor(Math.random() * hashtags.length)];
+  var quote = quoteComment[Math.floor(Math.random() * quoteComment.length)];
   //generate the search object:
   var hashtagSearch = {
     q: hash,
-    count: 10,
+    count: 20,
     result_type: 'recent'
   };
   console.log(hash);
+
+
+
+  // Search twitter using the randomly chosen hashtag:
+
   T.get('search/tweets', hashtagSearch, function (error, data) {
-    var tweets = data.statuses
-    if (data.statuses[0] != undefined) {
+
+
+    var tweets = data.statuses.filter(data => data.user.screen_name != myScreenName); // remove any tweets already sent by you from search
+    console.log(data.statuses.length);
+    console.log(tweets.length);
+
+
+    var randomisedTweet = [Math.floor(Math.random() * tweets.length)]
+
+    console.log(tweets[randomisedTweet]);
+
+
+    if (tweets[randomisedTweet] != undefined) {
+
+
       for (var i = 0; i < tweets.length; i++) {
-        // console.log(tweets[i].text)
+        // potential option here to use for loop to extract from search list
       }
-      // If our search request to the server had no errors...
       if (!error) {
-        // ...then we grab the ID of the tweet we want to retweet...
-        var retweetId = data.statuses[0].id_str
-        // ...and then we tell Twitter we want to retweet it!
-        T.post('statuses/retweet/' + retweetId, {}, tweeted)
+        // ...grab the ID of the tweet we want to quote and get username of tweeter...
+        var retweetId = tweets[randomisedTweet].id_str;
+        var mention = tweets[randomisedTweet].user.screen_name;
+
+        console.log(retweetId);
+        console.log('@' + tweets[randomisedTweet].user.screen_name);
+
+
+        // Retweet
+        //  T.post('statuses/retweet/' + retweetId, {}, tweeted);
+
+        // Quote Tweet with @mention of original user
+        // T.post('statuses/update', {
+        // status: '@' + mention + ' ' + quote + ' https://twitter.com/TwitterDev/statuses/' + retweetId
+        //}, tweeted);
+
       }
       // However, if our original search request had an error, we want to print it out here.
       else {
@@ -106,12 +89,12 @@ function retweetLatest() {
 
 }
 
-// Make sure it worked!
+// Callback to make sure it worked!
 function tweeted(err, reply) {
   if (err !== undefined) {
     console.log(err.message);
   } else {
-    console.log('Tweeted: ', reply);
+    console.log('Tweeted sucessfully!');
   }
 }
 
@@ -120,3 +103,10 @@ retweetLatest()
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 12)
+
+
+
+
+function reportBouncer(arr) {
+  return arr.filter(Boolean);
+};
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: ''
+  consumer_key: 'b',
+  consumer_secret: 'l',
+  access_token: 'a',
+  access_token_secret: 'h'
 }
